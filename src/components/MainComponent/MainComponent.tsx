20216531c6c4ef0ca7e4707fde361680d1790d70
LavrL <llembersk@gmail.com>
2020-01-21 15:29:31 +0200

    added fetch from json

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -6,30 +6,59 @@ import { SearchComponent } from '../SearchComponent/SearchComponent';
 interface MainComponentProps {
 }
 
-export class MainComponent extends React.Component<MainComponentProps, {}> {
+interface BonusesData {
+    title: string,
+    description: string,
+    link: string,
+    promocode: string
+}
+interface IMainComponentState {
+    bonuses: BonusesData[],
+    serviss: string,
+    balance: number,
+    next_payout: number
+}
+
+export class MainComponent extends React.Component<MainComponentProps, IMainComponentState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            serviss: ''
+            serviss: '',
+            bonuses: [],
+            balance: 0,
+            next_payout: 0
         }
         this.changeHandler = this.changeHandler.bind(this);
         this.getPromocodes = this.getPromocodes.bind(this)
     }
     changeHandler(event: any) {
-        this.setState({ serviss: event.target.value })
+        this.setState({ serviss: event.target.value });
+        console.log('Typed - ', this.state.serviss);
     }
     componentDidMount() {
         this.getPromocodes();
     }
     getPromocodes() {
-        // fetch('/assets/data.json')
-        // .then(response => {
-        //     if (!response.ok) {
-        //         console.log('Error +', response.status)
-        //     }
-        //     return response.json()
-        // })
-        // .then(json => )
+        fetch('/assets/data.json')
+            .then(response => {
+                if (!response.ok) {
+                    console.log('Error +', response.status)
+                }
+                return response.json()
+            })
+            .then(bonuses =>
+                {this.setState({
+                    bonuses: bonuses.bonuses as BonusesData[],
+                    balance: bonuses.header.balance,
+                    next_payout: bonuses.header.next_payout
+                })
+                console.log('bonuses - ', this.state.bonuses);
+                console.log('bonuses.header.balance - ', this.state.balance);
+                console.log('bonuses.header.next_payout - ', this.state.next_payout); }
+            )
+            .catch(error => {
+                console.log('Error = ', error);
+            });
     }
 
     render() {
@@ -39,11 +68,11 @@ export class MainComponent extends React.Component<MainComponentProps, {}> {
                     <ul className="MainComponent__title-list">
                         <li className="MainComponent__title-item">
                             <p className="MainComponent__title-item-balanss">Баланс</p>
-                            <p className="MainComponent__title-item-price">213 920 ₽</p>
+                            <p className="MainComponent__title-item-price">{this.state.balance} ₽</p>
                         </li>
                         <li className="MainComponent__title-item">
                             <p className="MainComponent__title-item-balanss">К выплате</p>
-                            <p className="MainComponent__title-item-price">159 465 ₽</p>
+                            <p className="MainComponent__title-item-price">{this.state.next_payout} ₽</p>
                         </li>
                     </ul>
                 </div>
@@ -58,7 +87,14 @@ export class MainComponent extends React.Component<MainComponentProps, {}> {
                         </form>
                         <ButtonComponent text="Сбросить" />
                     </div>
-                    <SearchComponent />
+                    
+                    {this.state.bonuses.map((bonus, i ) =>{
+                        return <SearchComponent key={i}
+                                                title = {bonus.title}
+                                                description = {bonus.description}
+                                                link ={bonus.link}
+                                                promocode ={bonus.promocode} />
+                    })}
                 </div>
 
             </div >
--- src/components/SearchComponent/SearchComponent.tsx
+++ src/components/SearchComponent/SearchComponent.tsx
@@ -5,25 +5,25 @@ import { ButtonComponent } from '../ButtonComponent/ButtonComponent';
 import icon_copy from '../../assets/copy_icon.svg';
 
 interface SearchComponentProps {
-    // title: string,
-    // description: string,
-    // link: string,
-    // promocode: string
+    key: number,
+    title: string
+    description: string,
+    link: string,
+    promocode: string
 }
 
 export class SearchComponent extends React.Component<SearchComponentProps, {}> {
     render() {
-
         return (
             <div className="SearchComponent">
                 <div className="SearchComponent__item">
                     <div className="SearchComponent__info">
-                        <p className="SearchComponent__title">Nethouse</p>
-                        <span className="SearchComponent__description">Описание</span>
+                        <p className="SearchComponent__title">{this.props.title}</p>
+                        <span className="SearchComponent__description">{this.props.description}</span>
                     </div>
                     <div className="SearchComponent__discount">
                         <div className="SearchComponent__promo">
-                            <span className="SearchComponent__promocode">Промокод</span>
+                            <span className="SearchComponent__promocode">{this.props.promocode}</span>
                             <input className="SearchComponent__promocode_input" type="text" defaultValue="Travelpayouts" />
                             <a href="#"> <img className="SearchComponent__promocode_input-icon_copy" src={icon_copy} alt="icon_copy" /></a>
                         </div>
